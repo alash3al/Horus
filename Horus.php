15b4449ec86fffb41f03cb07d65fff7558dfd5e5
Mohammed Al Ashaal <m7medalash3al@gmail.com>
2016-07-08 12:51:01 +0200

    Update Horus.php

--- Horus.php
+++ Horus.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Horus "Core" - be simple :)
+ * Horus
  * 
  * This version will only work on PHP 7 and higher .
  *
@@ -8,9 +8,53 @@
  * @copyright   2014 - 2016 (c) Horus
  * @author      nfinity <nfinity.space>
  * @license     MIT LICENSE
- * @version     v14
+ * @version     v15
  */
-Class Horus extends stdClass {
+namespace Horus;
+
+/**
+ * stdClass
+ * 
+ * An improved version of PHP stdClass .
+ *
+ * @package     Horus
+ * @author      nfinity <nfinity.space>
+ * @license     MIT LICENSE
+ * @version     v1.0
+ */
+class stdClass extends \stdClass {
+    /** @ignore */
+    public function __call($name, $args) {
+        if ( isset($this->{$name}) ) {
+            if ( $this->{$name} instanceof Closure ) {
+                return call_user_func_array($this->{$name}->bindTo($this), $args);
+            }
+            return call_user_func_array($this->{$name}, $args);
+        }
+        throw new \Exception("Undefined method {$name}");
+    }
+
+    /** @ignore */
+    public function __set($name, $val) {
+        if ( $val instanceof \Closure ) {
+            $this->{$name} = $val->bindTo($val);
+        } else {
+            $this->{$name} = $val;
+        }
+    }
+}
+
+/**
+ * App
+ * 
+ * This version will only work on PHP7 and higher .
+ *
+ * @package     Horus
+ * @author      nfinity <nfinity.space>
+ * @license     MIT LICENSE
+ * @version     v1.0
+ */
+class App extends stdClass {
     /**
      * An instance of this class
      * @var Horus
@@ -18,24 +62,19 @@ Class Horus extends stdClass {
     private static $instance;
 
     /**
-     * Whether to break the routes chain or not
-     * @var bool
+     * Router patterns shortcuts
+     * @var array
      */
-    private $breakRoutes;
-
-    /**
-     * The parent of the current route
-     * @var string
-     */
-    private $parent;
+    private $shortcuts;
 
     /**
      * Constructor
      *
-     * @param   string  $index
+     * @param   array  $configs
      */
     public function __construct(array $configs = []) {
     	static::$instance = $this;
+        $this->shortcuts = [];
         $this->configs = (object) array_merge(["index" => "/", "secure" => null], $configs);
     	$_SERVER["PATH_INFO"] = explode("?", $_SERVER["REQUEST_URI"])[0] ?? $_SERVER["REQUEST_URI"];
     	$strip = "/";
@@ -45,35 +84,17 @@ Class Horus extends stdClass {
     		$strip = dirname($_SERVER["SCRIPT_NAME"]);
     	}
     	$_SERVER["PATH_INFO"] = preg_replace("~/+~", "/", "/" . substr($_SERVER["PATH_INFO"], strlen($strip)) . "/");
-        $this->parent = "/";
-    }
-
-    /** @ignore */
-    public function __call($name, $args) {
-    	if ( isset($this->{$name}) ) {
-            if ( $this->{$name} instanceof Closure ) {
-                return call_user_func_array($this->{$name}->bindTo($this), $args);
-            }
-    		return call_user_func_array($this->{$name}, $args);
-    	}
-    	throw new Exception("Undefined method {$name}");
-    }
-
-    /** @ignore */
-    public function __set($name, $val) {
-    	if ( $val instanceof Closure ) {
-    		$this->{$name} = $val->bindTo($val);
-    	} else {
-    		$this->{$name} = $val;
-    	}
     }
 
     /**
      * Return horus instance
      * 
-     * @return  Horus
+     * @return  App
      */
     public static function getInstance() {
+        if ( ! static::$instance instanceof App ) {
+            return new App;
+        }
     	return static::$instance;
     }
 
@@ -100,6 +121,43 @@ Class Horus extends stdClass {
     }
 
     /**
+     * Whether the current request maybe under https or not
+     * 
+     * @return  bool
+     */
+    public function secure() {
+        if ( null !== $this->configs->secure && $this->configs->secure ) {
+            $_SERVER["HTTPS"] = "on";
+        }
+        if ( isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] !== "off") ) {
+            return true;
+        }
+        return isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && (strtolower($_SERVER["HTTP_X_FORWARDED_PROTO"]) == "https");
+    }
+
+    /**
+     * return a url to the specified path
+     * 
+     * @return  string
+     */
+    public function url($path = "") {
+        return (
+            $this->secure() ? "https://" : "http://" .
+            $_SERVER["SERVER_NAME"] .
+            preg_replace("~/+~", "/", "/" . dirname($_SERVER["SCRIPT_NAME"]) . "/" . $path . "/")
+        );
+    }
+
+    /**
+     * return a route to the specified path
+     * 
+     * @return  string
+     */
+    public function route($path = "") {
+        return $this->url($this->configs->index . "/" . $path);
+    }
+
+    /**
      * Make class in the specified dir(s) be autoloaded
      * 
      * @param   string|array    $src
@@ -121,12 +179,12 @@ Class Horus extends stdClass {
     					$found = false;
     				} else {
                         $found = true;
-    					require_once $filename;
+    					require_once($filename);
                         return ;
     				}
     			}
                 if ( ! $found ) {
-                    throw new Exception("Cannot find the class '{$class_orig}'");
+                    throw new \Exception("Cannot find the class '{$class_orig}'");
                 }
     		});
     	}
@@ -134,118 +192,48 @@ Class Horus extends stdClass {
     }
 
     /**
-     * Whether the current request maybe under https or not
+     * Create patterns shortcuts
      * 
-     * @return  bool
+     * @param   array   $shortcuts
+     * @return  $this
      */
-    public function secure() {
-        if ( null !== $this->configs->status && $this->configs->status ) {
-            $_SERVER["HTTPS"] = "on";
+    public function shortcut(array $shortcuts) {
+        foreach ( $shortcuts as $k => $v ) {
+            $this->shortcuts[sprintf("{{%s}}", $k)] = $v;
         }
-        if ( isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] !== "off") ) {
-            return true;
-        }
-        return isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && (strtolower($_SERVER["HTTP_X_FORWARDED_PROTO"]) == "https");
-    }
-
-    /**
-     * Return an url for a local path
-     * 
-     * @param   string  $path
-     * @param   array   $options
-     * @param   bool    $secure
-     * @return  string
-     */
-    public function url($path = "", $host = "", $secure = null) {
-        $secure = null === $secure ? $this->secure() : $secure;
-        $scheme = $secure ? "https://" : "http";
-        $host = $host ?? $_SERVER["SERVER_NAME"];
-        return $scheme . $host . preg_replace("~/+~", "/", "/" . $path);
-    }
-
-    /**
-     * Return an url for a local route
-     * 
-     * @param   string  $path
-     * @param   array   $options
-     * @param   bool    $secure
-     * @return  string
-     */
-    public function route($path = "", $host = "", $secure = null) {
-        return $this->url($this->configs->index . "/" . $path, $host, $secure);
+        return $this;
     }
 
     /**
      * Listen on the requested uri
      * 
-     * @param   string      $pattern
-     * @param   Callback    $cb
+     * @param   string                  $pattern
+     * @param   callback|[]callback     $cb
      * @return  $this
      */
-    public function on(string $pattern, callable $cb) {
-    	if ( $this->breakRoutes === true ) {
-    		return $this;
-    	}
-    	$parts = explode(" ", $pattern, 2);
-        if ( sizeof($parts) < 2 ) {
-            $method = $_SERVER["REQUEST_METHOD"];
-        } else {
-            $method = $parts[0];
-            $pattern = $parts[1];
-        }
-        $pattern = preg_replace("~/+~", "/", "/" . $this->parent . "/" . $pattern . "/");
-    	if ( ! preg_match("~^{$method}$~i", $_SERVER["REQUEST_METHOD"]) ) {
-    		return $this;
-    	} else if ( ! preg_match("~^{$pattern}$~", $_SERVER["PATH_INFO"], $m) ) {
+    public function on(string $pattern, $cb) {
+    	list($method, $pattern) = array_pad(explode(" ", $pattern, 2), -2, $_SERVER["REQUEST_METHOD"]);
+        $pattern = preg_replace("~/+~", "/",  "/" . str_ireplace(array_keys($this->shortcuts), array_values($this->shortcuts), $pattern) . "/");
+    	if ( ! preg_match("~^{$method}$~i", $_SERVER["REQUEST_METHOD"]) || ! preg_match("~^{$pattern}$~", $_SERVER["PATH_INFO"], $m) ) {
     		return $this;
     	}
     	array_shift($m);
-    	if ( call_user_func_array($cb->bindTo($this), $m) !== true ) {
-            // the callback didn't return true "continue"
-            // but returned none-true "stop" .
-    		$this->breakRoutes = true;
-    	}
-    	return $this;
-    }
-
-    /**
-     * Group some routes under the same pattern without the need to repeate anything
-     * 
-     * @param   string      $pattern
-     * @param   Callback    $cb
-     * @return  $this
-     */
-    public function group($pattern, callable $cb) {
-        $old = $this->parent;
-        $this->parent = preg_replace("~/+~", "/", "/" . $this->parent . "/" . $pattern . "/");
-        if ( preg_match("~^" . $this->parent . "~", $_SERVER["PATH_INFO"], $m) ) {
-            call_user_func_array($cb->bindTo($this), $m);
-        }
-        $this->parent = $old;
-        return $this;
-    }
-
-    /**
-     * Output the specified filename(s) to the browser and optionally pass vars to it/them
-     * 
-     * @param   string  $tpls
-     * @param   array   $ctx
-     * @param   bool    $return
-     * @return  $this|string
-     */
-    public function tpl($tpls, array $ctx = [], $return = false) {
-        if ( $return ) {
-            ob_start();
-        }
-    	extract($ctx, EXTR_OVERWRITE);
-    	foreach ( (array) $tpls as $v ) {
-    		if ( ! is_file($v) ) {
-    			throw new Exception("Template file cannot be found '{$v}'");
-    		}
-    		require $v;
-    	}
-        if ( $return ) {
-            return ob_get_clean();
+        $call = function($obj, $callback, $args){
+            if ( $callback instanceof \Closure ) {
+                return call_user_func_array($callback->bindTo($obj), $args);
+            } else {
+                return call_user_func_array($callback, $args);
+            }
+        };
+        $lastReturn = null;
+    	if ( is_callable($cb) ) {
+            $call($this, $cb, $m);
+        } else if ( is_array($cb) ) {
+            foreach ( $cb as $fn ) {
+                if ( false === ($lastReturn = $call($this, $fn, array_merge($m, [$lastReturn]))) ) {
+                    break;
+                }
+            }
         }
     	return $this;
     }
